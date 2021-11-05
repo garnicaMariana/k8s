6bd692cd0dfd32cf91773b24c1c5748d1efe8f59
Mariana Garnica Patricio <69858924+garnicaMariana@users.noreply.github.com>
2021-11-05 09:43:43 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Set Variables
+export AWS_REGION="us-east-2"
+export EKS_CLUSTER="eks-cluster-uT9o"
+
 # Upgrade OS
 sudo apt-get update
 sudo apt-get upgrade -y
@@ -9,12 +13,17 @@ sudo apt-get install -y unzip tree
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
-curl -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-sudo mv kubectl /usr/bin
 curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/aws-iam-authenticator
 chmod +x ./aws-iam-authenticator
 sudo mv aws-iam-authenticator /usr/bin
+
+# Install Kubectl
+# From AWS - specify version
+# curl -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.21.2/2021-07-05/bin/linux/amd64/kubectl
+# From K8S - always latest version
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv kubectl /usr/bin
 
 # Prepare Credentials
 mkdir ~/.aws
@@ -26,7 +35,7 @@ cat config > ~/.aws/config
 
 aws sts get-caller-identity
 # Two options to check: region and cluster name
-aws eks --region us-east-2 update-kubeconfig --name eks-cluster-uT9o
+aws eks --region $AWS_REGION update-kubeconfig --name $EKS_CLUSTER
 kubectl describe configmap -n kube-system aws-auth
 
 # Configure the auto completion for kubectl
@@ -41,5 +50,10 @@ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scr
 chmod 700 get_helm.sh
 sudo ./get_helm.sh
 
+# Cert-Manager
+curl -L -o kubectl-cert-manager.tar.gz https://github.com/jetstack/cert-manager/releases/latest/download/kubectl-cert_manager-linux-amd64.tar.gz
+tar xzf kubectl-cert-manager.tar.gz
+sudo mv kubectl-cert_manager /usr/local/bin
+
 # Clean temporary files
-rm -rf creds.txt EKS_Certificate awscliv2.zip get_helm.sh config credentials
+rm -rf creds.txt EKS_Certificate awscliv2.zip config credentials kubectl-cert-manager.tar.gz LICENSES README.md get_helm.sh
